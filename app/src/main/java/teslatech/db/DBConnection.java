27dae84fd0c4daaa7f4db12ddddbf489f2bf814d
Pieers <Piers_84@hotmail.com>
2024-10-27 23:06:43 -0500

    mysql server user change

--- app/src/main/java/teslatech/db/DBConnection.java
+++ app/src/main/java/teslatech/db/DBConnection.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 
 public class DBConnection {
     public static final String URL = "jdbc:mysql://localhost:3306/tesla_tech_db";
-    public static final String USER = "root";
-    public static final String PASSWORD = "#iwant6xanax";
+    public static final String USER = "pieers";
+    public static final String PASSWORD = "";
 
     public static Connection connectDB() {
         Connection connection = null;
