f2f9ecfeda7a23fd7c0a98b173b43b0411e8056e
Pieers <Piers_84@hotmail.com>
2024-10-27 22:51:16 -0500

    add connection with mysql db

---
+++ app/src/main/java/teslatech/db/DB.java
@@ -0,0 +1,10 @@
+package teslatech.db;
+
+public class DB {
+    public static String username;
+    public static String path;
+    public static String date;
+
+    public static Integer id;
+    public static Integer cID;
+}
---
+++ app/src/main/java/teslatech/db/DBConnection.java
@@ -0,0 +1,23 @@
+package teslatech.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    public static final String URL = "jdbc:mysql://localhost:3306/tesla_tech_db";
+    public static final String USER = "root";
+    public static final String PASSWORD = "#iwant6xanax";
+
+    public static Connection connectDB() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+
+        return connection;
+    }
+}
