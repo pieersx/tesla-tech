fd79c8d1b085e15cc625222d659191fec0146f61
Pieers <Piers_84@hotmail.com>
2024-11-27 22:55:59 -0500

    add db

--- app/src/main/java/teslatech/db/ConexionDB.java
+++ app/src/main/java/teslatech/db/ConexionDB.java
@@ -4,11 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+// mysql://root:boSfRwFPvOHlElpHSFRvOtiWhFJIEsmG@autorack.proxy.rlwy.net:54386/railway
+
 public class ConexionDB {
     // Constantes para la URL de la base de datos y las credenciales
-    public static final String URL = "jdbc:mysql://localhost:3306/tesla_tech_db";
-    public static final String USUARIO = "pieers";
-    public static final String CONTRASENA = "";
+    public static final String URL = "jdbc:mysql://autorack.proxy.rlwy.net:54386/tesla_tech_db";
+    public static final String USUARIO = "root";
+    public static final String CONTRASENA = "boSfRwFPvOHlElpHSFRvOtiWhFJIEsmG";
 
     public static Connection conectarDB() {
         Connection connection = null;
